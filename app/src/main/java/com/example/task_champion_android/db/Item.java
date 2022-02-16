bfcda2066951cd4c627dd8baab0276f4cb1155f3
Rohan <roh987gh@yahoo.com>
2022-02-15 19:16:34 -0500

    added update and create category method with dialog

--- app/src/main/java/com/example/task_champion_android/db/Item.java
+++ app/src/main/java/com/example/task_champion_android/db/Item.java
@@ -3,6 +3,7 @@ package com.example.task_champion_android.db;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "item_table")
 public class Item {
